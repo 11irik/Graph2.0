68f899c1b4bfbfee1d43c9171730fc6a1e7aeb7e
11irik <lukashinkv@gmail.com>
2019-09-16 18:57:21 +0400

    TestTestTest

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Node {
+    private Map<Node, Node> neighbours = new HashMap<Node, Node>();
+
+    private void addNeighbour(Node node){
+
+    }
+}
