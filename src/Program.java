286596d3fa2986b43c8cda473c9d4a3d76cc5f82
11irik <lukashinkv@gmail.com>
2019-09-23 12:41:15 +0400

    Some start addings

--- src/Program.java
+++
@@ -1,9 +0,0 @@
-public class Program {
-    public static void main(String[] args) {
-        Graph a = new Graph();
-        a.addNeighbour("hel", "llo");
-        a.addNeighbour("hel", "blo");
-
-        a.show();
-    }
-}
---
+++ src/main/java/Edge.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Edge {
+    private Node from;
+    private Node to;
+    private Integer weight;
+
+    public Edge(){
+        from = new Node();
+        to = new Node();
+        weight = 0;
+    }
+
+    public Edge(Node a, Node b, Integer w){
+        from = a;
+        to = b;
+        weight = w;
+    }
+
+    public Edge(Node a, Node b){
+        from = a;
+        to = b;
+        weight = 0;
+    }
+}
